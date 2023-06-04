6145b9cb64b78476ede0f7926abd58af396e5565
zaur <zaur.cherchesov@mail.ru>
2023-06-04 15:14:03 +0300

    Added method name: NewArrayWithCondition

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] NewArrayWithCondition(string[] array)
+{
+    // int new_size = 0;
+
+    string[] new_array = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 && array[i] != null)
+        {
+           new_array[i] += array[i];
+            // new_size++;
+        }
+    }
+    return new_array.Where(x => x != null).ToArray();
+}
+
