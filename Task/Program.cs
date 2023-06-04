d45bf5a42da4916f5be72e4b36c400ff68ee25a5
zaur <zaur.cherchesov@mail.ru>
2023-06-04 15:22:31 +0300

    Added method name: GenerateStringElement. Added Fill and Print array elements.

--- Task/Program.cs
+++ Task/Program.cs
@@ -14,3 +14,53 @@
     return new_array.Where(x => x != null).ToArray();
 }
 
+string GenerateStringElement()
+{
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=[]{}|<>.,/?~` ";
+
+    var random = new Random();
+    var stringChars = new char[random.Next(1, 8)];
+
+    for (int i = 0; i < stringChars.Length; i++)
+    {
+        stringChars[i] = chars[random.Next(chars.Length)];
+    }
+
+    return new String(stringChars);
+}
+
+void FillArray(string[] elements)
+{
+
+    {
+        for (int i = 0; i < elements.Length; i++)
+        {
+            elements[i] = GenerateStringElement();
+        }
+    }
+}
+
+void PrintArray(string[] elements)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < elements.Length; i++)
+    {
+        System.Console.Write($"\"{elements[i]}\", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+void Task()
+{
+    int size = 10;
+    string[] elements = new string[size];
+    FillArray(elements);
+    Console.WriteLine("Исходный массив:");
+    PrintArray(elements);
+    string[] resultArray = NewArrayWithCondition(elements);
+    Console.WriteLine("Результирующий массив:");
+    PrintArray(resultArray);
+
+
+}
+Task();
